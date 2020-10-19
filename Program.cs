65ea8076a40d22e2a82cd1963b2a39f2210048f6
iremxblm <69584205+iremxblm@users.noreply.github.com>
2020-10-19 17:24:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Metod
+    {
+        static void Yaz()
+        {
+            Console.Write("DENE");
+
+            
+  
+        static void Ciz();
+
+        {
+           Console.Write("\n");
+
+        }
+    static void Main ()
+    {
+        Yaz();
+    }
+    }
+
+
+
+
+    }
+}
